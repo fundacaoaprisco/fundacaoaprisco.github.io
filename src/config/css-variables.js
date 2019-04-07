3a124db8ac51aef8c776f6b0d88438e1593edc6a
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-06 21:25:34 -0300

    postcss variables

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -1,13 +1,22 @@
-let cssVariables = {};
+let cssVariables = {}
+
+const typography = {
+  'font-title': `'IBM Plex Serif', sans-serif`,
+  'font-text': `'Overpass', serif`,
+}
 
 const colors = {
-  black: "#333",
-  gray: "#666",
-  green: "#0d9999"
-};
+  black: '#333',
+  gray: '#666',
+  green: '#0d9999',
+  'color-primary': 'var(--green)',
+  'color-title': 'var(--black)',
+  'color-text': 'var(--gray)',
+}
 
 cssVariables = {
-  ...colors
-};
+  ...colors,
+  ...typography,
+}
 
-module.exports = { ...cssVariables };
+module.exports = { ...cssVariables }
