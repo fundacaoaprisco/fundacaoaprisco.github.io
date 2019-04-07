8d4b42d96e7e9e0e467079f55eba5c8ee1cb4da7
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-06 21:23:53 -0300

    updating components paths

---
+++ src/components/atoms/index.js
@@ -0,0 +1,3 @@
+export { default as Button } from './button'
+export { default as Container } from './container'
+export { default as Logo } from './logo'
---
+++ src/components/molecules/index.js
@@ -0,0 +1,1 @@
+export { default as LinkTo } from './link'
