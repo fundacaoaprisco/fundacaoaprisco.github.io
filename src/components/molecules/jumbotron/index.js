69df9cdc26f4ff3a90d10bef04a42a5d48bd6999
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-14 01:45:10 -0300

    jumbotron component

--- src/components/molecules/jumbotron/index.js
+++ src/components/molecules/jumbotron/index.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
+import { Heading, Text, Button } from '@atoms'
+
 import styles from './styles.module.css'
 
-const Jumbrotron = () => <div className={styles.jumbrotron} />
+const Jumbotron = () => (
+  <div className={styles.jumbotron}>
+    <Heading type="h1">
+      Comunidade que se
+      <br /> propõe a fazer o bem.
+    </Heading>
+    <Text className={styles.jumbotronText} white>
+      Promovemos projetos sociais voltados à educação,
+      <br /> cultura, inclusão social e geração de renda.
+    </Text>
+    <Button white>Veja nossos projetos</Button>
+  </div>
+)
+
+export default Jumbotron
