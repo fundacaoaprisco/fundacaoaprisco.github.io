d3774133e1d89e3d299c2d529a8c003caf28a389
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-14 01:44:44 -0300

    add ...props to button

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -3,8 +3,10 @@ import classNames from 'classnames'
 
 import styles from './styles.module.css'
 
-const Button = ({ children, white }) => (
-  <button className={classNames(styles.btn, { [styles.white]: white })}>{children}</button>
+const Button = ({ children, white, ...props }) => (
+  <button className={classNames(styles.btn, { [styles.white]: white })} {...props}>
+    {children}
+  </button>
 )
 
 export default Button
