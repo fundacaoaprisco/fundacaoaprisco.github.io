2f1bddb2bd8db186afbcbecfc600f8ac7b11684c
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-14 01:45:43 -0300

    starting home page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import { Button, Container } from '@atoms'
+import { Container } from '@atoms'
+import { Jumbotron, Card, SubtitleBar } from '@molecules'
 import { Navbar } from '@organisms'
 
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
 export default ({ data }) => (
   <Container>
     <Navbar />
-    <div className={styles.green}>
-      Hello {data.site.siteMetadata.title}! <Button>xablau</Button>
-    </div>
+    <Jumbotron />
+    <SubtitleBar text="Últimos posts" />
+    <Card
+      content={{
+        title: 'Infocentro',
+        text:
+          'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
+        image: 'https://picsum.photos/350/300',
+        href: '#',
+        button: 'Leia mais',
+      }}
+    />
   </Container>
 )
 
