ca74f179ae94b5d5bb5c94e481b224178a31983c
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-04-22 12:45:31 -0300

    project list components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,79 +1,44 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import { Container } from '@atoms'
-import { Jumbotron, Card, SubtitleBar } from '@molecules'
-import { Navbar } from '@organisms'
 
-import { Row, Col } from 'react-simple-flex-grid'
-import 'react-simple-flex-grid/lib/main.css'
+import { Jumbotron } from '@molecules'
+
+import { Navbar, ProjectsList } from '@organisms'
 
 // import styles from './styles.module.css'
 
-export default ({ data }) => (
+const PROJECTS = [
+  {
+    title: 'Infocentro',
+    text:
+      'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
+    image: 'https://picsum.photos/350/300',
+    href: '/',
+    button: 'Leia mais',
+  },
+  {
+    title: 'Infocentro',
+    text:
+      'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
+    image: 'https://picsum.photos/350/300',
+    href: '/',
+    button: 'Leia mais',
+  },
+  {
+    title: 'Infocentro',
+    text:
+      'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
+    image: 'https://picsum.photos/350/300',
+    href: '/',
+    button: 'Leia mais',
+  },
+]
+
+export default () => (
   <Container>
     <Navbar />
     <Jumbotron />
-    <SubtitleBar text="Últimos posts" />
-    <Row gutter={32}>
-      <Col span={3}>
-        <Card
-          content={{
-            title: 'Infocentro',
-            text:
-              'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
-            image: 'https://picsum.photos/350/300',
-            href: '#',
-            button: 'Leia mais',
-          }}
-        />
-      </Col>
-      <Col span={3}>
-        <Card
-          content={{
-            title: 'Infocentro',
-            text:
-              'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
-            image: 'https://picsum.photos/350/300',
-            href: '#',
-            button: 'Leia mais',
-          }}
-        />
-      </Col>
-      <Col span={3}>
-        <Card
-          content={{
-            title: 'Infocentro',
-            text:
-              'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
-            image: 'https://picsum.photos/350/300',
-            href: '#',
-            button: 'Leia mais',
-          }}
-        />
-      </Col>
-      <Col span={3}>
-        <Card
-          content={{
-            title: 'Infocentro',
-            text:
-              'O objetivo é fomentar o desenvolvimento profissional das camadas mais pobres de Feira de Santana, através de cursos básicos nas áreas de informática e internet.',
-            image: 'https://picsum.photos/350/300',
-            href: '#',
-            button: 'Leia mais',
-          }}
-        />
-      </Col>
-    </Row>
+    <ProjectsList projects={PROJECTS} />
   </Container>
 )
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
