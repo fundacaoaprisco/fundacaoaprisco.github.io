7d67ff6a12d693d8a44af5e5649a7389c8ec92b5
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2019-06-06 09:40:31 -0300

    feat: use eslint config

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import styles from './styles.module.css'
 
-const Button = ({ children, white, ...props }) => (
-  <button className={classNames(styles.btn, { [styles.white]: white })} {...props}>
+const Button = ({ children, white, type, onClick, className, ...props }) => (
+  <button type={type} onClick={onClick} className={classNames(styles.btn, { [styles.white]: white }, className)} {...props}>
     {children}
   </button>
 )
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  type: PropTypes.string,
+  white: PropTypes.bool,
+  className: PropTypes.string,
+}
+
+Button.defaultProps = {
+  type: 'button',
+  white: false,
+  onClick: () => {},
+  className: undefined,
+}
 
 export default Button
